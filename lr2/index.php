61fbd0ee579d739bde5910d198d848da62a8bea2
E2sembler <saven07@ya.ru>
2022-09-29 01:29:11 +0300

    главну страницу обновил

--- lr2/index.php
+++ lr2/index.php
@@ -1,8 +1,8 @@
-<?php $db = new PDO("sqlite:./laba2.db");?>
+
 
 <!DOCTYPE html>
 <html>
   <body>
-    <a href="./item/de7b1abb46123332/">click me</a>
+    <a href="./search/">click me</a>
   </body>
 </html>
