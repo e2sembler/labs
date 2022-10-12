dd94be0b0b682d23fe2230895bebe98194e07ca0
E2sembler <saven07@ya.ru>
2022-10-13 00:50:33 +0300

    duplicate

--- lr2/search/funcs.php
+++ lr2/search/funcs.php
@@ -43,46 +43,7 @@ function GetData($arr){
     $db=null;
     return $res;
     }
-function GetData1($arr){
-    $db = new PDO(conn,user,pass); 
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-     $array = array('fname'=>$arr['fname'],'fdesc'=>$arr['fdesc']);
-    $query = "SELECT Items.img_path,Items.name , Brands.name
-    ,Items.description,Items.price 
-    FROM Items 
-    INNER JOIN Brands on Items.id_brand=Brands.id 
-    WHERE Items.name like '%'||:fname||'%'
-    AND Items.description like '%'||:fdesc||'%' 
-    ";
-    
-    if(!empty($arr['fmax']) & is_numeric($arr['fmax'])){
-        $query.="AND Items.price <= :fmax ";
-        $array+=[':fmax'=>$arr['fmax']];}
-        
-    
-    if(!empty($arr['fmin']) & is_numeric($arr['fmin']))
-    {
-    $query.="AND Items.price >= :fmin ";
-    $array+=[':fmin'=>$arr['fmin']]; 
-    }
-    
-    if(!empty($arr['fbrand'])){
-    $query.="AND Items.id_brand=(SELECT id from Brands where name=:fbrand)";
-    $array+=[':fbrand'=>$arr['fbrand']];}
-    
-    $query.=" order by Items.price";       
-    $exec = $db->prepare($query);
-    try{
-    $exec->execute($array);}
-    catch(Exception $ex){
-        error_log($ex->getMessage());
-        exit();
-    } 
-    $res = $exec->fetchAll();
-    $db=null;
-    return $res;
-    }
+ 
 
 function GetBrands(){
  $db = new PDO(conn,user,pass); 
